f05146b61948729de0c63c93ac4f65a75b17a839
mgudurs84 <35053619+mgudurs84@users.noreply.github.com>
2019-07-25 11:31:56 -0400

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -1,6 +1,7 @@
 (function(window){
   window.extractData = function() {
     var ret = $.Deferred();
+    console.log(ret);
 
     function onError() {
       console.log('Loading error', arguments);
